916c773c4fe5e7ffbd51ea996e15eb9429f0c09e
Henry Morgan <76470485+henatic@users.noreply.github.com>
2024-03-12 14:36:35 -0700

    Going through tutorial at https://learn.microsoft.com/en-us/powershell/dsc/tutorials/create-class-based-resource?view=dsc-2.0

---
+++ ExampleResources/ExampleResources.psm1
@@ -0,0 +1,40 @@
+[DscResource()]
+class Tailspin {
+
+    [DscProperty(Key)] [TailspinScope]
+    $ConfigurationScope
+
+    [DscProperty()] [TailspinEnsure]
+    $Ensure = [TailspinEnsure]::Present
+
+    [DscProperty(Mandatory)] [bool]
+    $UpdateAutomatically
+
+    [DscProperty()] [int] [ValidateRange(1, 90)]
+    $UpdateFrequency
+
+    hidden [Tailspin] $CachedCurrentState
+    hidden [PSCustomObject] $CachedData
+
+    [Tailspin] Get() {
+        $CurrentState = [Tailspin]::new()
+        return $CurrentState
+    }
+
+    [bool] Test() {
+        $InDesiredState = $true
+        return $InDesiredState
+    }  
+
+    [void] Set() {}
+}
+
+enum TailspinScope {
+    Machine
+    User
+}
+
+enum TailspinEnsure {
+    Absent
+    Present
+}
